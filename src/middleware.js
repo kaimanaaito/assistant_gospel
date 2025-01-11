920345719edf157c51d26f88dc5db3087499a043
Aito Iida <kaimanaaito123q@gmail.com>
2025-01-11 11:16:08 -0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -6,9 +6,10 @@ export const config = {
 
 export function middleware(request) {
   const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-createxyz-project-id", "bade9e95-80ac-4929-a5d1-a416005c30c3");
+  requestHeaders.set("Authorization", `Bearer ${process.env.OPENAI_API_KEY}`);
+  requestHeaders.set("Content-Type", "application/json");
 
-  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+  request.nextUrl.href = `https://api.openai.com/${request.nextUrl.pathname}`;
 
   return NextResponse.rewrite(request.nextUrl, {
     request: {
